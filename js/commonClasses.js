597e082ed89958009cc2607cfe7ea582a3b18b5f
Richy <rjfox321@gmail.com>
2015-02-11 23:55:20 +0000

    Initial commit

---
+++ js/commonClasses.js
@@ -0,0 +1,39 @@
+﻿//Container for object boundaries
+function Bounds(pMinX, pMinY, pMaxX, pMaxY) {
+    this.minX = pMinX;
+    this.minY = pMinY;
+    this.maxX = pMaxX;
+    this.maxY = pMaxY;
+}
+
+
+function doBoundsIntersect(obj1Bounds, obj2Bounds) {
+
+    return (obj1Bounds.minX <= obj2Bounds.maxX) &&
+        (obj1Bounds.minY <= obj2Bounds.maxY) &&
+        (obj1Bounds.maxX >= obj2Bounds.minX) &&
+        (obj1Bounds.maxY >= obj2Bounds.minY);
+
+
+}
+
+
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+    var words = text.split(" ");
+    var line = "";
+
+    for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var metrics = context.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > maxWidth) {
+            context.fillText(line, x, y);
+            line = words[n] + " ";
+            y += lineHeight;
+        }
+        else {
+            line = testLine;
+        }
+    }
+    context.fillText(line, x, y);
+}
